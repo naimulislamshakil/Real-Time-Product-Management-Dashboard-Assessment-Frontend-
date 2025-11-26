59c4576d3a8729f162017cb6fd228d7064224f6e
Naimul Islam <ms0413354@gmail.com>
2025-11-26 07:03:18 +0600

    add

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+	children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+	return (
+		<div>
+			<main>{children}</main>
+		</div>
+	);
+};
+
+export default Layout;
